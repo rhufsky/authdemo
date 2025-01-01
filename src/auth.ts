d524c81ed86dcd4ea8d76d2dede4167e8f679748
HUO <robert.hufsky@htl-villach.at>
2025-01-01 18:00:18 +0100

    add types

--- src/auth.ts
+++ src/auth.ts
@@ -21,3 +21,19 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     },
   },
 });
+
+declare module "next-auth" {
+  interface Session {
+    error?: "RefreshTokenError";
+  }
+}
+
+declare module "next-auth/jwt" {
+  interface JWT {
+    access_token: string;
+    expires_at: number;
+    refresh_token?: string;
+    error?: "RefreshTokenError";
+    status: "INITIAL" | "REFRESH";
+  }
+}
